275fb0a0e9f1e551f4cdbba29f4cce7461e75df5
chloé Gattino <chloe.gattino@efrei.net>
2023-06-08 15:57:56 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,4 +1,16 @@
-#test
-import numpy as np
+import pandas as pd
 
-eonvoirnvoirngvo'itngotng'
+# Créer une liste avec les humeurs positives
+humeurs_positives = ["heureux", "enthousiaste", "épanoui", "optimiste", "serein"]
+
+# Créer une liste avec les humeurs négatives
+humeurs_negatives = ["triste", "déprimé", "anxieux", "frustré", "irrité"]
+
+# Créer un DataFrame à partir des listes d'humeurs
+df = pd.DataFrame({
+    "Humeur": humeurs_positives + humeurs_negatives,
+    "Sentiment": ["Positive"] * len(humeurs_positives) + ["Negative"] * len(humeurs_negatives)
+})
+
+# Afficher le DataFrame
+print(df)
