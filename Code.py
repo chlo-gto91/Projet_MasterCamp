ce638d8b016cd7500ada166343dd14f77ed1fbbe
chloé Gattino <chloe.gattino@efrei.net>
2023-06-08 17:00:17 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,23 +1,10 @@
 import pandas as pd
-<<<<<<< HEAD
+
 
 #%% Dictionnaire
-=======
-"""
-# Créer une liste avec les humeurs positives
-humeurs_positives = ["heureux", "enthousiaste", "épanoui", "optimiste", "serein"]
->>>>>>> d04389752062aab72360a806e7fa385a914f4eb7
 
 sujet_abordé = ["site internet","personnel", "libraison", "délai", "esthétique", "materiel","prix","établissement","matière","taille","politesse","produit","commande","service clients"]
-    
-
-
-<<<<<<< HEAD
-=======
-print(df)
-"""
+   
 
 # Retour attendu machine learning :
 dic={"content":["P",2],"desastre":["N",2]}
-
->>>>>>> d04389752062aab72360a806e7fa385a914f4eb7
