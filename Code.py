fba0e6a480e0fb50cea1ad4b669bb9ce8efd7b3d
chloé Gattino <chloe.gattino@efrei.net>
2023-06-09 12:29:23 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,9 +1,47 @@
 import pandas as pd
 
 
-#%% Dictionnaire
+#%% PARTIE MOMO
 
-sujet_abordé = ["site internet","personnel", "libraison", "délai", "esthétique", "materiel","prix","établissement","matière","taille","politesse","produit","commande","service clients"]
+
+humeurs_positives = ["heureux", "enthousiaste", "épanoui", "optimiste", "serein"]
+humeurs_negatives = ["triste", "déprimé", "anxieux", "frustré", "irrité"]
+inverseur_linguistique = ["pas"]
+adverbe = ["très", "peu", "beaucoup"]
+
+ 
+
+
+def pos_ou_neg(commentaire) : 
+
+    mot = string(map(if mot in humeurs_negatives || humeurs_positives))
+    if mot in humeurs_positives : 
+        return "humeur positive"
+    else :
+        return "humeur négative"
+
+def inverseur() : 
+    if inverseur_linguistique in : 
+        return "oui"
+    else : 
+        return "non"
+
+bool_inv = inverseur()
+
+ 
+# Créer un DataFrame à partir des listes d'humeurs
+df = pd.DataFrame({
+    "Humeur": humeurs_positives + humeurs_negatives,
+    "Sentiment": ["Positive"] * len(humeurs_positives) + ["Negative"] * len(humeurs_negatives)
+    "Inverseur": ["Oui"]
+})
+
+# Afficher le DataFrame
+df
+
+#%% PARTIE ANALYSE SUJET
+
+sujet_abordé = ["site internet","personnel", "livraison", "délai", "esthétique", "materiel","prix","établissement","matière","taille","politesse","produit","commande","service clients"]
    
 
 # Retour attendu machine learning :
